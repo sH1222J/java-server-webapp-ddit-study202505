49dee94fe0476519e5dbfdde4511add3028026d4
sH. <28824568+sH1222J@users.noreply.github.com>
2025-05-13 09:53:10 +0900

    2

---
+++ src/main/java/kr/or/ddit/dto/ArticleForm.java
@@ -0,0 +1,13 @@
+package kr.or.ddit.dto;
+
+// 자바빈 클래스 : 자바빈 규약을 지키는 클래스
+//              1) 프로퍼티 2) 기본생성자 3) getter/setter메소드
+public class ArticleForm {
+    // 제목을 받을 필드
+    private String title;
+    // 내용을 받을 필드
+    private String content;
+
+    public ArticleForm() {
+    }
+}
